41f75334f28ce55e57fd788bbe6ab026adf63d3d
Abhinav <abhinav.star@gmail.com>
2025-07-16 03:28:46 +0530

    Added gitignore file and fixed iam code issue

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,9 +1,11 @@
 resource "google_service_account" "composer" {
+  project      = var.project_id
   account_id   = "composer-sa"
   display_name = "Composer Service Account"
 }
 
 resource "google_project_iam_member" "composer_roles" {
+  project = var.project_id
   for_each = toset([
     "roles/composer.worker",
     "roles/storage.admin"
