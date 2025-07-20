41d9d6e1b7f8c2256e171a5e8c7694893235c4a6
abdul87s <124002160+abdul87s@users.noreply.github.com>
2025-07-21 04:26:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "dev-terraform-state-bucket-12"  # Replace with your GCS bucket name
+    bucket  = "dev-terraform-state-bucket-12"
     prefix  = "composer/bootstrap"
   }
 }
